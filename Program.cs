f1f5bba3b8ed1a6837c33063d7c24b197e3c6fe6
paul ryan urbano <paulryanurbano1989@gmail.com>
2023-06-22 17:41:38 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+global using Microsoft.EntityFrameworkCore;
+global using SuperHeroApis.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroApis
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string PLace { get; set; } = string.Empty;
+
+    }
+}
